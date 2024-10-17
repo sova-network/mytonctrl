1c2d66949eab79859bb0e47816fe0e805605fea6
Olexander Hofman <o@mevton.com>
2024-10-17 22:59:21 +0100

    add debug logs

--- scripts/install.sh
+++ scripts/install.sh
@@ -137,7 +137,7 @@ echo -e "${COLOR}[4/5]${ENDC} Running mytoninstaller"
 parent_name=$(ps -p $PPID -o comm=)
 user=$(whoami)
 if [ "$parent_name" = "sudo" ] || [ "$parent_name" = "su" ] || [ "$parent_name" = "python3" ]; then
-    user=$(logname)
+    user=$(whoami)
 fi
 echo "User: $user"
 echo "python3 -m mytoninstaller -u ${user} -t ${telemetry} --dump ${dump} -m ${mode}"
